84a313d1e4bf79c3746c4820c7d314ef722b3266
vsteffen <vsteffen@student.42.fr>
2019-01-30 18:17:54 +0100

    Modify Colleen and Grace comments to respect the subject's restrictions | Modify .gitignore for binaries and files generated

--- Grace/C/Grace.c
+++ Grace/C/Grace.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-#define SOURCE "#include <stdio.h>%c#define SOURCE %c%s%c%c#define FPRINTF(fd) fprintf(fd, SOURCE, 10, 34, SOURCE, 34, 10, 10, 34, 34, 34, 34, 10, 10, 10, 10, 10)%c#define GRACE int main(void) {FILE *fd; fd = fopen(%cGrace_kid.c%c,%cw%c); if (fd == NULL) return (1); FPRINTF(fd); fclose(fd); return (0);}%c%c/* I'm useful*/%c%cGRACE%c"
-#define FPRINTF(fd) fprintf(fd, SOURCE, 10, 34, SOURCE, 34, 10, 10, 34, 34, 34, 34, 10, 10, 10, 10, 10)
+#define SOURCE "#include <stdio.h>%c#define SOURCE %c%s%c%c#define FPRINTF(fd) fprintf(fd, SOURCE, 10, 34, SOURCE, 34, 10, 10, 34, 34, 34, 34, 10, 10, 10, 9, 10, 10, 10, 10)%c#define GRACE int main(void) {FILE *fd; fd = fopen(%cGrace_kid.c%c,%cw%c); if (fd == NULL) return (1); FPRINTF(fd); fclose(fd); return (0);}%c%c/*%c%cI'm useful%c*/%c%cGRACE%c"
+#define FPRINTF(fd) fprintf(fd, SOURCE, 10, 34, SOURCE, 34, 10, 10, 34, 34, 34, 34, 10, 10, 10, 9, 10, 10, 10, 10)
 #define GRACE int main(void) {FILE *fd; fd = fopen("Grace_kid.c","w"); if (fd == NULL) return (1); FPRINTF(fd); fclose(fd); return (0);}
 
-/* I'm useful*/
+/*
+	I'm useful
+*/
 
 GRACE
