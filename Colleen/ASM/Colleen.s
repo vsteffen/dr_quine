a4f4b6d425fdd8ff4232ca149e258974bd845a81
vsteffen <vsteffen@student.42.fr>
2019-02-04 16:23:55 +0100

    Add '\0' for Colleen source code string

--- Colleen/ASM/Colleen.s
+++ Colleen/ASM/Colleen.s
@@ -25,4 +25,4 @@ ret
 
 
 section .data
-source: db "global  _main%2$cextern  _printf%2$c%2$csection .text%2$c%2$c; This is gonna be amazing%2$c%2$c_main:%2$cpush rbp%2$cmov rbp, rsp%2$c; Very cool function below%2$c%2$cprint_source_code:%2$clea rdi, [rel source]%2$clea rsi, [rel source]%2$cmov rdx, 10%2$cmov rcx, 34%2$ccall _printf%2$c%2$cexit:%2$cmov rsp, rbp%2$cpop rbp%2$cxor rax, rax%2$cret%2$c%2$c%2$csection .data%2$csource: db %3$c%1$s%3$c%2$c"
+source: db "global  _main%2$cextern  _printf%2$c%2$csection .text%2$c%2$c; This is gonna be amazing%2$c%2$c_main:%2$cpush rbp%2$cmov rbp, rsp%2$c; Very cool function below%2$c%2$cprint_source_code:%2$clea rdi, [rel source]%2$clea rsi, [rel source]%2$cmov rdx, 10%2$cmov rcx, 34%2$ccall _printf%2$c%2$cexit:%2$cmov rsp, rbp%2$cpop rbp%2$cxor rax, rax%2$cret%2$c%2$c%2$csection .data%2$csource: db %3$c%1$s%3$c, 0%2$c", 0
