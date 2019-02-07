f663990bd27e1bc392631bc5ecee89203081e0c4
vsteffen <vsteffen@student.42.fr>
2019-02-07 19:55:56 +0100

    Sully quine work now properly with -1

--- C/Sully/Sully.c
+++ C/Sully/Sully.c
@@ -16,12 +16,12 @@ int	main(void)
 	FILE *fd;
 	char *file;
 	char *file_exec;
-	char *source = "#include <stdio.h>%c#include <libgen.h>%c#include <stdlib.h>%c#include <string.h>%c%cvoid%cfree_alloc(char *file, char *file_exec, char *command)%c{%c%cfree(file);%c%cfree(file_exec);%c%cfree(command);%c}%c%cint%cmain(void)%c{%c%cint i = %d;%c%cFILE *fd;%c%cchar *file;%c%cchar *file_exec;%c%cchar *source = %c%s%c;%c%cchar *command;%c%c%cif (!strstr(getenv(%c_%c), %cSully_%c))%c%c%ci++;%c%cif (i < 0)%c%c%creturn (1);%c%c%casprintf(&file, %c%%s/Sully_%%d.c%c, dirname(__FILE__), --i);%c%cfd = fopen(file, %cw%c);%c%cif (fd == NULL)%c%c{%c%c%cfree(file);%c%c%creturn (1);%c%c}%c%cfprintf(fd, source, 10, 10, 10, 10, 10, 9, 10, 10, 9, 10, 9, 10, 9, 10, 10, 10, 9, 10, 10, 9, i, 10, 9, 10, 9, 10, 9, 10, 9, 34, source, 34, 10, 9, 10, 10, 9, 34, 34, 34, 34, 10, 9, 9, 10, 9, 10, 9, 9, 10, 10, 9, 34, 34, 10, 9, 34, 34, 10, 9, 10, 9, 10, 9, 9, 10, 9, 9, 10, 9, 10, 9, 10, 9, 10, 10, 9, 10, 9, 92, 10, 9, 34, 34, 10, 9, 10, 9, 9, 10, 9, 10, 9, 10, 10);%c%cfclose(fd);%c%c%cfile_exec = strdup(file);%c%cfile_exec[strlen(file_exec) - 2] = '%c0';%c%casprintf(&command, %cclang -Wall -Wextra -Werror %%s -o Sully_%%d ; %%s%c, file, i, file_exec);%c%cif (i > 0)%c%c%ci = system(command);%c%cfree_alloc(file, file_exec, command);%c%creturn (i);%c}%c";
+	char *source = "#include <stdio.h>%c#include <libgen.h>%c#include <stdlib.h>%c#include <string.h>%c%cvoid%cfree_alloc(char *file, char *file_exec, char *command)%c{%c%cfree(file);%c%cfree(file_exec);%c%cfree(command);%c}%c%cint%cmain(void)%c{%c%cint i = %d;%c%cFILE *fd;%c%cchar *file;%c%cchar *file_exec;%c%cchar *source = %c%s%c;%c%cchar *command;%c%c%cif (!strstr(getenv(%c_%c), %cSully_%c))%c%c%ci++;%c%cif (i <= 0)%c%c%creturn (1);%c%c%casprintf(&file, %c%%s/Sully_%%d.c%c, dirname(__FILE__), --i);%c%cfd = fopen(file, %cw%c);%c%cif (fd == NULL)%c%c{%c%c%cfree(file);%c%c%creturn (1);%c%c}%c%cfprintf(fd, source, 10, 10, 10, 10, 10, 9, 10, 10, 9, 10, 9, 10, 9, 10, 10, 10, 9, 10, 10, 9, i, 10, 9, 10, 9, 10, 9, 10, 9, 34, source, 34, 10, 9, 10, 10, 9, 34, 34, 34, 34, 10, 9, 9, 10, 9, 10, 9, 9, 10, 10, 9, 34, 34, 10, 9, 34, 34, 10, 9, 10, 9, 10, 9, 9, 10, 9, 9, 10, 9, 10, 9, 10, 9, 10, 10, 9, 10, 9, 92, 10, 9, 34, 34, 10, 9, 10, 9, 9, 10, 9, 10, 9, 10, 10);%c%cfclose(fd);%c%c%cfile_exec = strdup(file);%c%cfile_exec[strlen(file_exec) - 2] = '%c0';%c%casprintf(&command, %cclang -Wall -Wextra -Werror %%s -o Sully_%%d ; %%s%c, file, i, file_exec);%c%cif (i > 0)%c%c%ci = system(command);%c%cfree_alloc(file, file_exec, command);%c%creturn (i);%c}%c";
 	char *command;
 
 	if (!strstr(getenv("_"), "Sully_"))
 		i++;
-	if (i < 0)
+	if (i <= 0)
 		return (1);
 
 	asprintf(&file, "%s/Sully_%d.c", dirname(__FILE__), --i);
